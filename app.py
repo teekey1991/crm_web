3926d3b719a51a7a5dfda37ae7a54d0672f574cb
teekey1991 <matsunaetaiki@gmail.com>
2022-07-03 12:17:04 +0900

    完成

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from config import Customer
+app = Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    # customers = [
+    #     Customer(name="Bob", age=79),
+    #     Customer(name="Tom", age=57),
+    #     Customer(name="Ken", age=73),
+    # ]
+    customers = Customer.select()
+    return render_template("index.html", customers=customers)
+
+@app.route("/add",methods=["POST"])
+def add():
+    name = request.form["name"]
+    age = request.form["age"]
+
+    Customer.create(name=name, age=age) #インスタンスを作らずにセーブできる
+
+    # return redirect("/index")
+    return redirect(url_for("index"))
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
